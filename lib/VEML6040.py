70c8ccbda048069899c1704ab8d0a682ef9e6fe2
kendra-eck <206059012+kendra-eck@users.noreply.github.com>
2026-06-19 13:33:46 +0200

    Update VEML6040 library

--- lib/VEML6040.py
+++ lib/VEML6040.py
@@ -1,4 +1,7 @@
 """
+###################################################################################
+# Edited by Joy-IT
+###################################################################################
 Author: Core Electronics Pty Ltd.
 https://github.com/CoreElectronics/CE-PiicoDev-VEML6040-MicroPython-Module/tree/main
 
@@ -28,18 +31,52 @@ import utime
 
 class VEML6040:
 
-    CONF_REG = 0x00
-    RED_REG = 0x08
-    GREEN_REG = 0x09
-    BLUE_REG = 0x0A
-    WHITE_REG = 0x0B
+    REG_CONF = 0x00
+    REG_RED = 0x08
+    REG_GREEN = 0x09
+    REG_BLUE = 0x0A
+    REG_WHITE = 0x0B
+    
+    INTEGRATION_TIME_40MS = 0
+    INTEGRATION_TIME_80MS = 1
+    INTEGRATION_TIME_160MS = 2
+    INTEGRATION_TIME_320MS = 3
+    INTEGRATION_TIME_640MS = 4
+    INTEGRATION_TIME_1280MS = 5
 
-    def __init__(self, i2c, address=0x10):
+    _INTEGRATION_TIME_VALUES = {
+        INTEGRATION_TIME_40MS: 0x00,
+        INTEGRATION_TIME_80MS: 0x10,
+        INTEGRATION_TIME_160MS: 0x20,
+        INTEGRATION_TIME_320MS: 0x30,
+        INTEGRATION_TIME_640MS: 0x40,
+        INTEGRATION_TIME_1280MS: 0x50,
+    }
+
+    _INTEGRATION_TIME_DELAY = {
+        INTEGRATION_TIME_40MS: 40,
+        INTEGRATION_TIME_80MS: 80,
+        INTEGRATION_TIME_160MS: 160,
+        INTEGRATION_TIME_320MS: 320,
+        INTEGRATION_TIME_640MS: 640,
+        INTEGRATION_TIME_1280MS: 1280,
+    }
+
+    BIT_SD = 0x01
+    BIT_AF = 0x02
+    BIT_TRIG = 0x04
+    MASK_INTEGRATION_TIME = 0x70
+
+    def __init__(self, i2c, integration_time=INTEGRATION_TIME_160MS, address=0x10):
         """
         initialize colour sensor
         i2c - established i2c connection
+        integration_time - 
         address - i2c addresss of color sensor
         """
+        if integration_time not in self._INTEGRATION_TIME_VALUES:
+            raise ValueError("integration_time is not in range")
+        self.integration_time = integration_time
         self.i2c = i2c
         self.address = address
         self.enableSensor()
@@ -51,7 +88,13 @@ class VEML6040:
         reg - register
         value - value to be written
         """
-        self.i2c.writeto_mem(self.address, reg, bytearray([value]))
+        data = bytes([
+            reg & 0xFF,
+            value & 0xFF,
+            (value >> 8) & 0xFF,
+        ])
+        
+        self.i2c.writeto_mem(self.address, reg, data)
         
 
     def _read_register(self, reg):
@@ -59,101 +102,139 @@ class VEML6040:
         read value from register on the colour sensor
         reg - register
         """
-        return int.from_bytes(self.i2c.readfrom_mem(self.address, reg, 2), 'little')
+        register = bytes([reg & 0xFF])
+        last_error = None
+        
+        for _ in range(3):
+            try:
+                return int.from_bytes(self.i2c.readfrom_mem(self.address, reg, 2), 'little')
+            except OSError as error:
+                last_error = error
+                utime.sleep_ms(20)
+        raise last_error
 
 
     def enableSensor(self):
         """
         activate sensor
         """
-        conf = self._read_register(self.CONF_REG) & 0x00FE
-        self._write_register(self.CONF_REG, conf)
+        config = self._INTEGRATION_TIME_VALUES[self.integration_time]
+        config &= ~self.BIT_SD
+        config &= ~self.BIT_AF
+        config &= ~self.BIT_TRIG
+        self._write_register(self.REG_CONF, config)
+        utime.sleep_ms(self._INTEGRATION_TIME_DELAY[self.integration_time])
         
         
     def disableSensor(self):
         """
         deactivate sensor
         """
-        conf = (self._read_register(self.CONF_REG) & 0x00FE) | 0x00
-        self._write_register(self.CONF_REG, conf)
-        utime.sleep_ms(1)
+        config = self._read_register(self.REG_CONF)
+        config |= self.BIT_SD
+        self._write_register(self.REG_CONF, config)
         
         
-    def setIntegrationTime(self, int_time):
+    def setIntegrationTime(self, integration_time):
         """
-        set integration time with variable int_time
-        int_time - integration time 
-            0 = 40 ms
-            1 = 80 ms
-            2 = 160 ms
-            3 = 320 ms
-            4 = 640 ms
-            5 = 1280 ms
+        set integration time with variable integration_time
+        integration_time - _INTEGRATION_TIME_VALUES 
+            INTEGRATION_TIME_40MS = 40 ms
+            INTEGRATION_TIME_80MS = 80 ms
+            INTEGRATION_TIME_160MS = 160 ms
+            INTEGRATION_TIME_320MS = 320 ms
+            INTEGRATION_TIME_640MS = 640 ms
+            INTEGRATION_TIME_1280MS = 1280 ms
         """
-        if int_time < 0 or int_time > 5:
-            raise ValueError('int_time is not in range')
-        conf = self._read_register(self.CONF_REG) & 0x0003
-        self._write_register(self.CONF_REG, (conf | (int_time << 4)))
+        if integration_time not in self._INTEGRATION_TIME_VALUES:
+            raise ValueError("integration_time is not in range")
+        config = self._read_register(self.REG_CONF)
+        config &= ~self.MASK_INTEGRATION_TIME
+        config |= self._INTEGRATION_TIME_VALUES[integration_time]
+        config &= ~self.BIT_SD
+        self.integration_time = integration_time
+        self._write_register(self.REG_CONF, config)
+        utime.sleep_ms(self._INTEGRATION_TIME_DELAY[self.integration_time])
         
     
     def forceMode(self):
         """
         forces measurement mode - triggers to start
         """
-        conf = (self._read_register(self.CONF_REG) & 0x0072) | 0x0002
-        self._write_register(self.CONF_REG, conf)
+        config = self._read_register(self.REG_CONF)
+        config &= self.MASK_INTEGRATION_TIME
+        config |= self.BIT_AF
+        config |= self.BIT_TRIG
+        config &= ~self.BIT_SD
+        self._write_register(self.REG_CONF, config)
+        utime.sleep_ms(self._INTEGRATION_TIME_DELAY[self.integration_time])
         
         
     def autoMode(self):
         """
         automatic measurement mode
         """
-        conf = self._read_register(self.CONF_REG) & 0x0070
-        self._write_register(self.CONF_REG, conf)
+        config = self._read_register(self.REG_CONF)
+        config &= self.MASK_INTEGRATION_TIME
+        config &= ~self.BIT_AF
+        config &= ~self.BIT_TRIG
+        config &= ~self.BIT_SD
+        self._write_register(self.REG_CONF, config)
+        utime.sleep_ms(self._INTEGRATION_TIME_DELAY[self.integration_time])
         
         
     def get_red(self):
         """
         returns raw value of red
         """
-        return self._read_register(self.RED_REG)
+        return self._read_register(self.REG_RED)
     
 
     def get_green(self):
         """
         returns raw value of green
         """
-        return self._read_register(self.GREEN_REG)
+        return self._read_register(self.REG_GREEN)
     
 
     def get_blue(self):
         """
         returns raw value of blue
         """
-        return self._read_register(self.BLUE_REG)
+        return self._read_register(self.REG_BLUE)
     
 
     def get_white(self):
         """
         returns raw value of white
         """
-        return self._read_register(self.WHITE_REG)
+        return self._read_register(self.REG_WHITE)
     
 
     def get_rgbw(self):
         """
         returns raw value of all RGBW values
         """
-        return self.get_red(), self.get_green(), self.get_blue(), self.get_white()
+        red = self.get_red()
+        utime.sleep_ms(5)
+        green = self.get_green()
+        utime.sleep_ms(5)
+        blue = self.get_blue()
+        utime.sleep_ms(5)
+        white = self.get_white()
+        return red, green, blue, white
 
 
     def readAll(self):
         """
         returns most recognized colour and raw values
         """
-        r, g, b, w = self.get_rgbw()
-        raw_val = [r, g, b, w]
-        dominant_col, dominant_val = "red", r
-        if b > dominant_val: dominant_col, dominant_val = "blue", b
-        if g > dominant_val: dominant_col, dominant_val = "green", g
-        return dominant_col, raw_val
+        red, green, blue, white = self.get_rgbw()
+        values = {
+            "red": red,
+            "green": green,
+            "blue": blue,
+        }
+
+        dominant_color = max(values, key=values.get)
+        return dominant_color, [red, green, blue, white]
